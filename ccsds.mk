e85450deacb273a36034efec68b58c2ac07352ec
Miles Simpson <miles.a.simpson@gmail.com>
2021-05-13 09:19:54 -0700

    Add initial make include file

---
+++ ccsds.mk
@@ -0,0 +1,8 @@
+CCSDS_ROOT ?= .
+
+CCSDS_INC =	$(CCSDS_ROOT)/include
+CCSDS_SRC =	$(CCSDS_ROOT)/frame_buf.c	\
+			$(CCSDS_ROOT)/uslp.c		\
+			$(CCSDS_ROOT)/cop.c			\
+			$(CCSDS_ROOT)/spp.c			\
+			$(CCSDS_ROOT)/sdls.c
