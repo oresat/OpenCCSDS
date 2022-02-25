cd0259910c4bcf5f099b4da3194544013aaee698
Miles Simpson <miles.a.simpson@gmail.com>
2022-02-25 13:30:46 -0800

    Only add SDLS when appropriate

--- src/sdls.c
+++ src/sdls.c
@@ -39,14 +39,14 @@ int sdls_send(const sdls_cfg_t *cfg, fb_t *fb)
     void *seq_num = NULL;
     void *mac = NULL;
 
-    fb_push(fb, cfg->pad_len);
-    seq_num = fb_push(fb, cfg->seq_num_len);
-    iv = fb_push(fb, cfg->iv_len);
-    spi = fb_push(fb, sizeof(uint16_t));
-    mac = fb_put(fb, cfg->mac_len);
-    *spi = __builtin_bswap16(cfg->spi);
-
     if (cfg->send_func != NULL) {
+        fb_push(fb, cfg->pad_len);
+        seq_num = fb_push(fb, cfg->seq_num_len);
+        iv = fb_push(fb, cfg->iv_len);
+        spi = fb_push(fb, sizeof(uint16_t));
+        mac = fb_put(fb, cfg->mac_len);
+        *spi = __builtin_bswap16(cfg->spi);
+
         ret = cfg->send_func(fb->data, fb->len - cfg->mac_len, iv, seq_num, mac, cfg->send_arg);
     }
 
@@ -67,19 +67,20 @@ int sdls_recv(const sdls_cfg_t *cfg, fb_t *fb)
         uint8_t     mac[cfg->mac_len];
     } *tlr;
 
-    hdr = (struct sdls_hdr*)fb->data;
-    tlr = (struct sdls_tlr*)(fb->tail - cfg->mac_len);
-
-    if (__builtin_bswap16(hdr->spi) != cfg->spi) {
-        return -1;
-    }
-
     if (cfg->recv_func != NULL) {
+        hdr = (struct sdls_hdr*)fb->data;
+        tlr = (struct sdls_tlr*)(fb->tail - cfg->mac_len);
+
+        if (__builtin_bswap16(hdr->spi) != cfg->spi) {
+            return -1;
+        }
+
         ret = cfg->recv_func(fb->data, fb->len - cfg->mac_len, hdr->iv, hdr->seq_num, tlr->mac, cfg->recv_arg);
+
+        fb_pull(fb, sizeof(struct sdls_hdr));
+        fb_trim(fb, sizeof(struct sdls_tlr));
     }
 
-    fb_pull(fb, sizeof(struct sdls_hdr));
-    fb_trim(fb, sizeof(struct sdls_tlr));
     return ret;
 }
 
